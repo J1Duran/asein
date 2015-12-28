0479f7b6c2dcb061b3bf7d9178c1822b5c0edbf7
Jesus Duran <jduran@seguroscaroni.com>
2015-12-28 17:12:08 -0430

    Ultimas

--- contact_me.php
+++ contact_me.php
@@ -16,21 +16,21 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // create email body and send it	
-$to = 'duranbecool@gmail.com'; // PUT YOUR EMAIL ADDRESS HERE
+$to = 'info@asein.mx'; // PUT YOUR EMAIL ADDRESS HERE
 $email_subject = "Mensaje de:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
 $email_body = "Ha recibido un mensaje desde el formulario de contacto.\n\n"."Aqui estan los detalles:\n\nNombre: $name\n\nTelefono: $phone\n\nEmail: $email_address\n\nMensaje:\n$message";
-$headers = "From: noResponder@asein.esy.es\n";
+$headers = "From: noResponder@info@asein.mx\n";
 $headers .= "Reply-To: $email_address";	
 if(mail($to,$email_subject,$email_body,$headers))
 { 
   echo "<script language='javascript'>
       alert('Mensaje enviado, muchas gracias.');
-      window.location.href = 'http://asein.esy.es/prue/index.html';
+      window.location.href = 'http://www.asein.com.mx/index.html';
       </script>";
 } 
 else{ echo "<script language='javascript'>
       alert(Ha ocurrido un error intentelo de nuevo.');
-      window.location.href = 'http://asein.esy.es/prue/contacto.html';
+      window.location.href = 'http://www.asein.com.mx/contacto.html';
       </script>";
 }
 return true;			
